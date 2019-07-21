d424799f1db2f2fa0f7b49a08a08f726ff52f252
مهدی پرستار <m_parastar@cra.net>
2019-07-21 13:46:56 +0430

    npm test added

--- server/test/api_test.js
+++ server/test/api_test.js
@@ -1,5 +1,10 @@
 const assert = require('assert')
+const path = require('path');
 
+process.env.NODE_CONFIG_DIR = path.join(__dirname, '../config');
+const config = require('config');
+
+console.log('*****************   test   *******************', config.get('Level'))
 describe('User api', () => {
     it('Login', () => {
         assert(1 + 1 == 2)
