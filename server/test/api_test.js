b7cf3165fb0e221166d5b4c58adaa30ea0cdd37f
مهدی پرستار <m_parastar@cra.net>
2019-07-21 14:22:08 +0430

    npm test added

--- server/test/api_test.js
+++ server/test/api_test.js
@@ -1,10 +1,23 @@
 const assert = require('assert')
 const path = require('path');
-
+const { User } = require('../model/user');
 process.env.NODE_CONFIG_DIR = path.join(__dirname, '../config');
 const config = require('config');
 
-console.log('*****************   test   *******************', config.get('Level'))
+console.log('Level: ', config.get('Level'))
+
+describe('Create test User', () => {
+    it('Create', () => {
+        let testUser = new User({
+            fullname: 'test testy',
+            email: 'test.name@gmail.com',
+            password: 'testtest',
+            roles: ['admin', 'post_pishkhan_user']
+        })
+        testUser.save()
+    })
+})
+
 describe('User api', () => {
     it('Login', () => {
         assert(1 + 1 == 2)
