4adc93197ffc94ffc892efba5a4382b08e561431
مهدی پرستار <m_parastar@cra.net>
2019-07-24 16:19:42 +0430

    تکمیل کد های تقسیمات کشوری به همراه ایجاد روابط بین جدول هایشان (یک به چند و چند به یک) و ارسال به پایگاه داده مونگو

--- server/initializing/initializing.js
+++ server/initializing/initializing.js
@@ -3,9 +3,9 @@ const path = require('path');
 const excelToJson = require('convert-excel-to-json');
 const _ = require('lodash')
 const { Ostans, Shahrestans, Bakhshs, Shahrs, Dehestans, Abadis } = require('../model/CountryDivisions');
-const { One_to_Many_RelationShip } = require('../utils/utils')
 
 /////////////////////////////////////ایجاد یوزر اولیه در دیتابیس/////////////////////////////////////////////////////
+
 let testUser = new User({
     fullname: 'name family',
     email: 'family.name@gmail.com',
@@ -17,6 +17,7 @@ Promise.all([
 ]).then(() => console.log(' --***-- user initializing finished --***-- ')).catch((err) => console.log(err))
 
 //////////////////////////////خواندن اطلاعات تقسیمات کشوری از جداول اکسل و ذخیره در قالب جی سان///////////////////////////////////////////
+
 const ostans_init_Data = excelToJson({
     sourceFile: path.join(__dirname, '../initializing/CountryDivision_Data_to_initializing/ostans.xlsx'),
     header: { rows: 1 },
@@ -94,6 +95,9 @@ const abadis_init_Data = excelToJson({
         'F': '{{F1}}'
     }
 });
+
+//////////////////////////////////////ایجاد ارتباط بین جداول تقسیمات کشوری و ذخیره در پایگاه داده////////////////////////////////////
+
 ostans_list = []
 shahrestans_list = []
 bakhshs_list = []
@@ -103,65 +107,48 @@ abadis_list = []
 
 ostans_init_Data.ostans.forEach(ostan => {
     new_ostan = new Ostans(ostan)
-
     shahrestans_init_Data.shahrestans.forEach(shahrestan => {
         if (new_ostan.Ostan_id === shahrestan.Shahrestan_id.slice(0, 2)) {
             new_shahrestan = new Shahrestans(shahrestan)
+            new_shahrestan.ostan = new_ostan
             new_ostan.shahrestans.push(new_shahrestan)
-
             bakhshs_init_Data.bakhshs.forEach(bakhsh => {
-                if (new_shahrestan.shahrestan_id === bakhsh.Bakhsh_id.slice(0, 4)) {
+                if (new_shahrestan.Shahrestan_id === bakhsh.Bakhsh_id.slice(0, 4)) {
                     new_bakhsh = new Bakhshs(bakhsh)
+                    new_bakhsh.shahrestan = new_shahrestan
                     new_shahrestan.bakhshs.push(new_bakhsh)
                     shahrs_init_Data.shahrs.forEach(shahr => {
                         if (new_bakhsh.Bakhsh_id === shahr.Shahr_id.slice(0, 6)) {
                             new_shahr = new Shahrs(shahr)
+                            new_shahr.bakhsh = new_bakhsh
                             new_bakhsh.shahrs.push(new_shahr)
                             shahrs_list.push(new_shahr)
-
-                            // Promise.all([
-                            //     new_ostan.save(),
-                            //     new_shahrestan.save(),
-                            //     new_bakhsh.save(),
-                            //     new_shahr.save()
-                            // ])
                         }
                     })
                     dehestans_init_Data.dehestans.forEach(dehestan => {
-                        if (new_bakhsh.Bakhsh_id === dehestan.Dehestan_id.slice(0, 10)) {
+                        if (new_bakhsh.Bakhsh_id === dehestan.Dehestan_id.slice(0, 6)) {
                             new_dehestan = new Dehestans(dehestan)
+                            new_dehestan.bakhsh = new_bakhsh
                             new_bakhsh.dehestans.push(new_dehestan)
                             abadis_init_Data.abadis.forEach(abadi => {
-                                if (new_dehestan.Dehestan_id === abadi.Abadi_id) {
+                                if (new_dehestan.Dehestan_id === abadi.Abadi_id.slice(0, 10)) {
                                     new_abadi = new Abadis(abadi)
+                                    new_abadi.dehestan = new_dehestan
                                     new_dehestan.abadis.push(new_abadi)
                                     abadis_list.push(new_abadi)
-
-                                    // Promise.all([
-                                    //     new_ostan.save(),
-                                    //     new_shahrestan.save(),
-                                    //     new_bakhsh.save(),
-                                    //     new_dehestan.save(),
-                                    //     new_abadi.save()
-                                    // ])
                                 }
                             })
-
                             dehestans_list.push(new_dehestan)
                         }
                     })
-
                     bakhshs_list.push(new_bakhsh)
                 }
             })
-
             shahrestans_list.push(new_shahrestan)
         }
     })
-
     ostans_list.push(new_ostan)
 });
-
 
 Promise.all([
     console.log(' starting ...'),
@@ -172,95 +159,3 @@ Promise.all([
     Dehestans.collection.insertMany(dehestans_list).then((result) => { console.log('Dehestan Initialized') }).catch((err) => { console.log('Dehestan Exist') }),
     Abadis.collection.insertMany(abadis_list).then((result) => { console.log('Abadi Initialized') }).catch((err) => { console.log('Abadi Exist') }),
 ]).then(() => console.log(' --***-- country division initializing finished --***-- ')).catch((err) => console.log(err))
-
-//////////////////////////////////////ایجاد ارتباط بین جداول تقسیمات کشوری و ذخیره در پایگاه داده////////////////////////////////////
-
-// let ostan_dict = []
-// let shahrestan_dict = []
-// let bakhsh_dict = []
-// let dehestan_dict = []
-
-// ostan_shahrestans_One_side_object = {
-//     One_side_data: ostans_init_Data.ostans,
-//     One_side_Model: Ostans,
-//     Many_Side_One_field: 'ostan',
-//     One_Side_id_field: 'Ostan_id',
-// }
-// ostan_shahrestans_Many_side_objects = [{
-//     Many_side_data: shahrestans_init_Data.shahrestans,
-//     Many_side_Model: Shahrestans,
-//     One_Side_Many_field: 'shahrestans',
-//     Many_side_id_field: 'Shahrestan_id',
-//     Many_side_first_slice_index: 0,
-//     Many_side_second_slice_index: 2
-// }]
-// ostan_dict, shahrestans_many = One_to_Many_RelationShip(ostan_shahrestans_One_side_object, ostan_shahrestans_Many_side_objects)
-// console.log(shahrestans_many)
-// shahrestan_bakhshs_One_side_object = {
-//     One_side_data: shahrestans_many,
-//     One_side_Model: Shahrestans,
-//     Many_Side_One_field: 'shahrestan',
-//     One_Side_id_field: 'Shahrestan_id',
-// }
-// shahrestan_bakhshs_Many_side_objects = [{
-//     Many_side_data: bakhshs_init_Data.bakhshs,
-//     Many_side_Model: Bakhshs,
-//     One_Side_Many_field: 'bakhshs',
-//     Many_side_id_field: 'Bakhsh_id',
-//     Many_side_first_slice_index: 0,
-//     Many_side_second_slice_index: 4
-// }]
-// shahrestan_dict, bakhshs_many = One_to_Many_RelationShip(shahrestan_bakhshs_One_side_object, shahrestan_bakhshs_Many_side_objects)
-
-// bakhsh_shahrs_and_dehestans_One_side_object = {
-//     One_side_data: bakhshs_init_Data.bakhshs,
-//     One_side_Model: Bakhshs,
-//     Many_Side_One_field: 'bakhsh',
-//     One_Side_id_field: 'Bakhsh_id',
-// }
-// bakhsh_shahrs_and_dehestans_Many_side_objects = [{
-//     Many_side_data: shahrs_init_Data.shahrs,
-//     Many_side_Model: Shahrs,
-//     One_Side_Many_field: 'shahrs',
-//     Many_side_id_field: 'Shahr_id',
-//     Many_side_first_slice_index: 0,
-//     Many_side_second_slice_index: 6
-// }, {
-//     Many_side_data: dehestans_init_Data.dehestans,
-//     Many_side_Model: Dehestans,
-//     One_Side_Many_field: 'dehestans',
-//     Many_side_id_field: 'Dehestan_id',
-//     Many_side_first_slice_index: 0,
-//     Many_side_second_slice_index: 6
-// }]
-
-// dehestan_abadis_and_dehestans_One_side_object = {
-//     One_side_data: dehestans_init_Data.dehestans,
-//     One_side_Model: Dehestans,
-//     Many_Side_One_field: 'dehestan',
-//     One_Side_id_field: 'Dehestan_id',
-// }
-// dehestan_abadis_and_dehestans_Many_side_objects = [{
-//     Many_side_data: abadis_init_Data.abadis,
-//     Many_side_Model: Abadis,
-//     One_Side_Many_field: 'abadis',
-//     Many_side_id_field: 'Abadi_id',
-//     Many_side_first_slice_index: 0,
-//     Many_side_second_slice_index: 10
-// }]
-
-// bakhsh_dict = One_to_Many_RelationShip(bakhsh_shahrs_and_dehestans_One_side_object, bakhsh_shahrs_and_dehestans_Many_side_objects)
-// dehestan_dict = One_to_Many_RelationShip(dehestan_abadis_and_dehestans_One_side_object, dehestan_abadis_and_dehestans_Many_side_objects)
-
-
-// Promise.all([
-//     Abadis.collection.insertMany(abadis_init_Data.abadis).then((result) => { console.log('Abadi Initialized') }).catch((err) => { console.log('Abadi Exist') }),
-//     Dehestans.collection.insertMany(dehestan_dict).then((result) => { console.log('Dehestan Initialized') }).catch((err) => { console.log('Dehestan Exist') }),
-//     Shahrs.collection.insertMany(shahrs_init_Data.shahrs).then((result) => { console.log('Shahr Initialized') }).catch((err) => { console.log('Shahr Exist') }),
-//     Bakhshs.collection.insertMany(bakhsh_dict).then((result) => { console.log('Bakhsh Initialized') }).catch((err) => { console.log('Bakhsh Exist') }),
-//     Shahrestans.collection.insertMany(shahrestan_dict).then((result) => { console.log('Shahrestan Initialized') }).catch((err) => { console.log('Shahrestan Exist') }),
-//     Ostans.collection.insertMany(ostan_dict).then((result) => { console.log('Ostan Initialized') }).catch((err) => { console.log('Ostan Exist') }),
-// ]).then(() => console.log(' --***-- country division initializing finished --***-- ')).catch((err) => console.log(err))
-
-
-
--- server/utils/utils.js
+++ server/utils/utils.js
@@ -17,61 +17,7 @@ let asyncForEach = async (array, callback) => {
         await callback(array[index], index, array);
     }
 }
-///////////////////////////////////////////////ایجاد ارتباط یک به چند و چند به یک بین دو جدول///////////////////////////////////////////////
-let One_to_Many_RelationShip = (One_side_object, Many_side_objects) => {
-    // Many_side_objects = [{
-    //     Many_side_data,
-    //     Many_side_Model,
-    //     One_Side_Many_field,
-    //     Many_side_id_field,
-    //     Many_side_first_slice_index,
-    //     Many_side_second_slice_index
-    // }]
-    // One_side_object = {
-    //     One_side_data,
-    //     One_side_Model,
-    //     Many_Side_One_field,
-    //     One_Side_id_field,
-    // }
 
-    result = []
-    Many_list = []
-    One_side_object.One_side_data.forEach((One) => {
-        new_One = new One_side_object.One_side_Model(One)
-        result.push(new_One)
-
-        Many_side_objects.forEach((Many_side_object) => {
-            _.find(result, One)[Many_side_object.One_Side_Many_field] = []
-            Many_side_object.Many_side_data.forEach((Many) => {
-                if (One[One_side_object.One_Side_id_field] === Many[Many_side_object.Many_side_id_field].slice(Many_side_object.Many_side_first_slice_index, Many_side_object.Many_side_second_slice_index)) {
-                    new_Many = new Many_side_object.Many_side_Model(Many)
-                    _.find(Many_side_object.Many_side_data, Many)[One_side_object.Many_Side_One_field] = new_One._id
-                    _.find(result, One)[Many_side_object.One_Side_Many_field].push(new_Many)
-                    Many_list.push(new_Many)
-                }
-            })
-        })
-    })
-    return result, Many_list
-}
-//                 مثال دستی از تابع بالا
-// shahrestans_init_Data.shahrestans.forEach((shahrestan) => {
-//     new_Shahrestans = new Shahrestans(shahrestan)
-//     shahrestan_dict.push(new_Shahrestans)
-//     _.find(shahrestan_dict, shahrestan).bakhshs = []
-//     bakhshs_init_Data.bakhshs.forEach((bakhsh) => {
-//         if (shahrestan.Shahrestan_id === bakhsh.Bakhsh_id.slice(0, 4)) {
-//             new_Bakhshs = new Bakhshs(bakhsh)
-//             new_Bakhshs.shahrestan = new_Shahrestans
-//             _.find(bakhshs_init_Data.bakhshs, bakhsh)['shahrestan'] = new_Shahrestans._id
-//             _.find(shahrestan_dict, shahrestan).bakhshs.push(new_Bakhshs)
-//         }
-//     })
-// })
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////
 module.exports = {
     asyncForEach,
-    One_to_Many_RelationShip
 }
