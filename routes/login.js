31d9cb38bbff23337c2ba6a11ea4d4b6112a24d7
mehdi <parastar.mehdi@gmail.com>
2019-11-15 23:58:01 +0330

    ...

--- routes/login.js
+++ routes/login.js
@@ -5,10 +5,19 @@ const { User } = require('../model/user');
 
 router.post('/', async (req, res) => {
     try {
-        const { error } = await User.validateLogin(req);                
+        const { error } = await User.validateLogin(req);
         const findedUser = await User.findByCredentials(req);
         const token = await findedUser.generateAuthToken();
-        res.header('x-auth-token', token).status(200).send(token);
+        const user = {
+            firstName: findedUser.firstName,
+            lastName: findedUser.lastName,
+            email: findedUser.email,
+            userAvatar: findedUser.userAvatar,
+            orginizationRole: findedUser.orginizationRole,
+            userRoles: findedUser.userRoles,
+            token
+        }
+        res.header('x-auth-token', token).status(200).send(user);
     } catch (e) {
         res.status(400).json({
             Error: `Somethings went wrong. ${e}`
